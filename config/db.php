d73f49fff9a3212a6b30335ee4f6ab7852e724be
David Richard <davidrichard.ms@gmail.com>
2019-04-05 14:52:18 -0300

    first commit

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'development' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'precode',
+        'username' => 'precode',
+        'password' => 'precode',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci'
+    ]
+];
---
+++ phinx.php
@@ -0,0 +1,34 @@
+<?php 
+
+require __DIR__ . '/vendor/autoload.php';
+
+$db = include __dir__ . '/config/db.php';
+list(
+    'driver' => $adapter,
+    'host' => $host,
+    'database' => $name,
+    'username' => $user,
+    'password' => $pass,
+    'charset' => $charset,
+    'collation' => $collation
+) = $db['development'];
+return [
+    'paths' => [
+        'migrations' => [
+            __DIR__ . '/db/migrations'
+        ]
+    ],
+    'enviroments' => [
+        'default_migration_table' => 'migrations',
+        'default_database' => 'development',
+        'development' => [
+            'adapter' => $adpter,
+            'host' => $host,
+            'name' => $name,
+            'user' => $user,
+            'pass' => $pass,
+            'charset' => $charset,
+            'collation' => $collation
+        ]
+    ]
+];
